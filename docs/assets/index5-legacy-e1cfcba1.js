f2423523deb3e4a4f39f7700d34bededebddb481
Kaspars <kaspars@purens.lv>
2023-12-12 18:13:13 +0200

    test build

--- docs/assets/index5-legacy-e1cfcba1.js
+++
@@ -1,5 +0,0 @@
-System.register([],(function(e,n){"use strict";return{execute:function(){
-/*!
-			 * (C) Ionic http://ionicframework.com - MIT License
-			 */
-e("w","undefined"!=typeof window?window:void 0),e("d","undefined"!=typeof document?document:void 0)}}}));
