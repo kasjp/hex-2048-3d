5bbed1fa5f29db72e18f9a9528f23b1cc9d94314
Kaspars <kaspars@purens.lv>
2023-12-12 13:15:26 +0200

    feat: HexGrid get neighbor tile

--- src/game/engine/components/HexGrid.ts
+++ src/game/engine/components/HexGrid.ts
@@ -1,6 +1,6 @@
 import Helpers from "@Helpers";
-import { ExtendedGroup, Scene3D, THREE } from "@enable3d/phaser-extension";
-import { AssetMap, TAssetId } from "@engine/constants/AssetMap";
+import { ExtendedGroup, THREE } from "@enable3d/phaser-extension";
+import { TAssetId } from "@engine/constants/AssetMap";
 import AssetManager from "@engine/managers/AssetManager";
 import HexTile from "@engine/objects/HexTile";
 import HexScene from "@engine/scenes/HexScene";
@@ -8,12 +8,7 @@ import HexScene from "@engine/scenes/HexScene";
 export default class HexGrid extends ExtendedGroup {
   private _tilemap = new Map<string, HexTile>();
   private _tileSize = new THREE.Vector3(1, 1, 1);
-  constructor(
-    private _scene: HexScene,
-    private _size_x = 32,
-    private _size_z = 32,
-    private _size_y = 1
-  ) {
+  constructor(private _scene: HexScene, private _gridSize: number) {
     super();
     this._scene.third.add.existing(this);
   }
@@ -24,43 +19,33 @@ export default class HexGrid extends ExtendedGroup {
     this._tilemap.set(tile.tileIdString, tile);
   }
 
-  generateHexGrid(tileAssetId: TAssetId = "base", size = 2) {
-    size = size - 1;
-    for (let q = -size; q < size + 1; q++) {
-      for (let r = -size; r < size + 1; r++) {
+  generateHexGrid(tileAssetId: TAssetId = "base") {
+    for (let q = -this._gridSize + 1; q < this._gridSize; q++) {
+      for (let r = -this._gridSize + 1; r < this._gridSize; r++) {
         const s = -q - r;
-        if (Math.abs(q) <= size && Math.abs(r) <= size && Math.abs(s) <= size) {
+        if (
+          Math.abs(q) <= this._gridSize - 1 &&
+          Math.abs(r) <= this._gridSize - 1 &&
+          Math.abs(s) <= this._gridSize - 1
+        ) {
           this.createTileAt(q, r, tileAssetId);
         }
       }
     }
   }
-  // async generateFrom2DArray(
-  //   tileIdArray: Array<Array<TAssetId | undefined>>,
-  //   y = 0
-  // ) {
-  //   const promises = [];
-  //   for (let x = 0; x < tileIdArray.length - 1; x++) {
-  //     for (let z = 0; z < tileIdArray[x].length - 1; z++) {
-  //       if (tileIdArray[x][z])
-  //         promises.push(
-  //           HexTile.loadAndCreate(this, this._scene, tileIdArray[x][z]).then(
-  //             (tile) => {
-  //               tile.size = this._tileSize;
-  //               tile.setCoordinates(x, y, z);
-  //               this._tilemap.set(`${x}-${y}-${z}`, tile);
-  //             }
-  //           )
-  //         );
-  //     }
-  //   }
-  //   await Promise.all(promises);
-  // }
+
   getTiles() {
     return [...this._tilemap.values()];
   }
   getTileAt(q: number, r: number) {
     return this._tilemap.get(Helpers.Vector.toTileIdString({ x: q, y: r }));
+  }
+  getNeighborTile(
+    tile: HexTile,
+    directionKey: keyof typeof Helpers.Hex.DIRECTION_KEYS
+  ) {
+    const neighborCoords = tile.neighborCoordsByKey(directionKey);
+    return this.getTileAt(neighborCoords.x, neighborCoords.y);
   }
   getTileAtScreenXY(x: number, y: number): HexTile | undefined {
     let result: HexTile | undefined;
@@ -75,9 +60,7 @@ export default class HexGrid extends ExtendedGroup {
       });
     return result;
   }
-  shiftTiles(keyCode: number) {
-    /** */
-  }
+
   update(delta: number) {
     this._tilemap.forEach((tile) => tile.update(delta));
   }
