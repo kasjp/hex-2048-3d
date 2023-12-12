570d9cd6a7f8d33d1643d019395d61f3270fcaa3
Kaspars <kaspars@purens.lv>
2023-12-12 13:17:22 +0200

    cleanup

--- src/shared/Helpers/hex.ts
+++ src/shared/Helpers/hex.ts
@@ -7,7 +7,6 @@ namespace Hex {
     SW: "SW",
     SE: "SE",
   } as const;
-  export type DIRECTION_STRING = keyof typeof DIRECTION_STRING;
   export const ENABLED_KEYS = ["Q", "W", "A", "S", "Z", "X"] as const;
   export const DIRECTION_KEYS = {
     Q: "NW",
